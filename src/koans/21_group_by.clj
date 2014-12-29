d3bf4083f252a16dc111a8330a59611403109221
Derek Anderson <derek@armyofevilrobots.com>
2014-12-28 20:22:20 -0800

    You have achieved clojure enlightenment. Namaste.

--- src/koans/21_group_by.clj
+++ src/koans/21_group_by.clj
@@ -31,13 +31,16 @@
   "But be careful when you group by non-required key"
   (= {"Bob" [{:name "Bob" :id 1}]
       "Mike" [{:name "Mike" :id 2}]
-      "Smith" [{:last-name "Smith" :id 1}]}
+      nil [{:last-name "Smith" :id 1}]}
    (group-by :name [{:id 1 :name "Bob"}
                     {:id 2 :name "Mike"}
                     {:id 1 :last-name "Smith"}]))
 
   "The true power of group-by comes with custom functions"
-  (= __
+  (= {:nice-list [{:name "Jack" :bad false}]
+      :naughty-list [{:name "Jimmy" :bad true}
+                     {:name "Joe" :bad true}]}
+
      (group-by #(if (:bad %) :naughty-list :nice-list)
                [{:name "Jimmy" :bad true}
                 {:name "Jack" :bad false}
